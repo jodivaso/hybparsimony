f15010eb9c016fd8d5d8ca4a8adca8a26ec40eee
Jose Divasón <jose.divason@gmail.com>
2023-07-24 19:53:23 +0200

    more

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'HYBparsimony',         # How you named your package folder (MyLib)
   packages = ['HYBparsimony'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   python_requires='>=3.9',
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
