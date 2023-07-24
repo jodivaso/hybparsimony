75d283aa3e33f89955ba3bbb93bd1a2bf053e8e3
Jose Divasón <jose.divason@gmail.com>
2023-07-24 19:28:06 +0200

    preparing for PyPI

--- HYBparsimony/util/__init__.py
+++ HYBparsimony/util/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["fitness", "order", "population", "parsimony_monitor", "complexity", "config"]
+__all__ = ["complexity", "fitness", "hyb_aux", "models", "order", "parsimony_monitor", "population"]
 
 from .fitness import getFitness
 from .population import Population
 from .parsimony_monitor import parsimony_monitor, parsimony_summary
 from .order import order
 from .complexity import generic_complexity, linearModels_complexity, svm_complexity, knn_complexity, mlp_complexity, randomForest_complexity, xgboost_complexity
+from .models import check_algorithm, algorithms_dict
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'HYBparsimony',         # How you named your package folder (MyLib)
+  packages = ['HYBparsimony'],   # Chose the same as "name"
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  python_requires='>=3.9',
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Jose Divasón',                   # Type in your name
+  author_email = 'jose.divason@unirioja.es',      # Type in your E-Mail
+  url = 'https://github.com/jodivaso/HYBparsimony',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/jodivaso/HYBparsimony/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'scikit-learn',
+          'numpy',
+          'pandas',
+          'seaborn',
+          'matplotlib'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.9',
+  ],
+)
